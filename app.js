c49c3ae714cc6e5afb6a5cfe9d4d66812a4cbd15
mnuf23 <ennajaf18@alkadhum-col.edu.iq>
2023-09-22 01:03:33 +0300

    finsh

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-function calculateFactorial(number) {}
+function calculateFactorial(number) {
+ 
+    let factorial =1;
+   
+     for (let i = 1; i <= number; i++) {
+       
+       factorial = factorial * i;
+       console.log(`the factorial of ${i} = ${factorial}`);
+       
+       
+     }
+     console.log(`the full factorial is = ${factorial}`);
+   }
+   calculateFactorial(5);
